275082d923ffbb64e4a92a1a15bd1a6a931f306a
john kavanagh <johnkav1050@hotmail.com>
2015-02-22 23:53:09 +0000

    added commands in main to call gamescene

--- js/Main.js
+++ js/Main.js
@@ -3,10 +3,12 @@ function main()
 {
 	//change version number if you suspect a problem with caching
 	console.log("version 1");
+	sceneM = new SceneManager();
+
+	sceneM.goToScene("GameScene");
 
 	//assetManager = new AssetManager();
 	//assetManager.queueDownload("img/SnakeCreeping.png");
 	
 	//assetManager.downloadAll(function() {
-	
 }
