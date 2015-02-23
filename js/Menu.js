ab91c2e8a5b51fd0dc140fa138a0c00a97b94f7d
john kavanagh <johnkav1050@hotmail.com>
2015-02-23 11:20:04 +0000

    changes to menu

---
+++ js/Menu.js
@@ -0,0 +1,56 @@
+var ctx;
+var player;
+var androidPlayer;
+var score;
+
+function Menu (){
+	this.loopSound = new Audio("assets/music.mp3");
+	this.loopSound.volume = .0;
+}
+
+Game.prototype.initCanvas=function () { 
+	
+	var canvas = document.getElementById("canvas");
+	ctx = canvas.getContext("2d");
+	width = canvas.getAttribute("width");
+	height = canvas.getAttribute("height");
+
+	if (touchable) {
+		canvas.addEventListener( 'touchstart', onTouchStart, false );
+	} else {
+		console.log("not touchable");
+		canvas.addEventListener("click", false);
+	};
+
+	this.loopSound.addEventListener('ended',function()
+	{
+		game.loopSound.currentTime=0;
+		game.loopSound.play();
+	},false);
+	
+	this.loopSound.play();
+}
+
+Game.prototype.update = function (){
+	if(KeyController.isKeyDown(Key.UP)){
+	}
+}
+
+function onTouchDown(e){
+	touches = e.touches;
+}
+
+Game.prototype.checkCollisions=function (){
+
+}
+
+Game.prototype.gameLoop = function () {
+	
+}
+
+Game.prototype.draw =function (){
+	ctx.fillStyle = "white";
+	ctx.fillRect(0, 0, width, height);
+	ctx.strokeStyle = "black";
+	ctx.strokeRect(0, 0, width, height);
+}
